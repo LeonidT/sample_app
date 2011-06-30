fa23b5d800f3188efc9c468010917143447b0a97
Leonid <leonid@tonybet.lt>
2011-06-30 11:32:07 +0300

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   match '/contact',   :to => 'pages#contact'
   match '/about',     :to => 'pages#about'
   match '/help',      :to => 'pages#help'
+  match '/signup',    :to => 'users#new'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
